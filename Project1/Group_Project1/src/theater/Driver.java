65a6ff45866259c046626e5b7efe3a6d482cdb6d
shagur <60166295+shagur@users.noreply.github.com>
2020-07-22 04:51:04 -0500

    Update Driver.java

--- Project1/Group_Project1/src/theater/Driver.java
+++ Project1/Group_Project1/src/theater/Driver.java
@@ -143,6 +143,8 @@ public class Driver {
 			} else if (ans == 13){
 
 				showAllCommands();
+			} else{
+				System.out.println("Input must be between 1-13, try again.");
 			}
 
 		} while (ans != 0);
