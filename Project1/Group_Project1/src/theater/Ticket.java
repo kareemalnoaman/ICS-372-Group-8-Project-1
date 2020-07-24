1b2a6230c05c5aeb87709664ca06296478bb00c5
shagur <60166295+shagur@users.noreply.github.com>
2020-07-24 08:33:12 -0500

    Ticket class

---
+++ Project1/Group_Project1/src/theater/Ticket.java
@@ -0,0 +1,18 @@
+package theater;
+
+import java.util.Date;
+
+public class Ticket {
+
+    private double price;
+    private int serialNum;
+    private Date showDate;
+
+
+    public Ticket(double price, int serialNum, Date showDate) {
+        this.price = price;
+        this.serialNum = serialNum;
+        this.showDate = showDate;
+    }
+    
+}
